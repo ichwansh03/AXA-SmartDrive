55002c38ae3dbe213f389981c1c66dc433334e0d
Tengku1 <anountengku@gmail.com>
2023-11-17 13:30:42 +0700

    First Initialize Spring Boot

---
+++ src/main/java/com/app/smartdrive/SmartDriveAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.smartdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDriveAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDriveAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/smartdrive/SmartDriveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.smartdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDriveAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
