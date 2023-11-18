f414feb27e69a9527b83a67a59775cd33cc847ae
bakterimu <>
2023-11-18 11:16:24 +0700

    wkwk

--- src/main/resources/databases/master/master.sql
+++ src/main/resources/databases/master/master.sql
@@ -6,6 +6,7 @@ CREATE TABLE mtr.category (
     cate_id INT IDENTITY(1,1) PRIMARY KEY,
     cate_name VARCHAR(55)
 );
+--wkwkwk
 
 CREATE TABLE mtr.insurance_type (
     inty_name VARCHAR(25) PRIMARY KEY,
