872058053bde7c0f62f2cc978fcb709dd65b7d77
bakterimu <>
2023-11-19 19:21:08 +0700

    add controller and servis testing business entity and child

---
+++ src/main/java/com/app/smartdrive/api/services/users/BusinessEntityService.java
@@ -0,0 +1,9 @@
+package com.app.smartdrive.api.services.users;
+
+import java.util.List;
+
+import com.app.smartdrive.api.entities.users.BusinessEntity;
+
+public interface BusinessEntityService {
+  List<BusinessEntity> getAll();
+}
