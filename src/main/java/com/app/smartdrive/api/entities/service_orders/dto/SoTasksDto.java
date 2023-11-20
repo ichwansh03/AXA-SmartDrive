07cf72bbacfdf8a80e73bd824c589f8acd3f1e8a
Ichwan <ichwansholihin03@gmail.com>
2023-11-20 07:48:12 +0700

    chore: add dto `ServiceOrderTasks` and `ServiceOrderWorkorder`

---
+++ src/main/java/com/app/smartdrive/api/entities/service_orders/dto/SoTasksDto.java
@@ -0,0 +1,23 @@
+package com.app.smartdrive.api.entities.service_orders.dto;
+
+import com.app.smartdrive.api.entities.service_orders.ServiceOrderWorkorder;
+import com.app.smartdrive.api.entities.service_orders.enumerated.EnumModuleServiceOrders;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SoTasksDto {
+
+    private Long taskId;
+    private String taskName;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private EnumModuleServiceOrders.SeotStatus status;
+    private Set<ServiceOrderWorkorder> serviceOrderWorkorderSet;
+}
---
+++ src/main/java/com/app/smartdrive/api/entities/service_orders/dto/SoWorkorderDto.java
@@ -0,0 +1,14 @@
+package com.app.smartdrive.api.entities.service_orders.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SoWorkorderDto {
+
+    private String sowoName;
+    private String sowoStatus;
+}
