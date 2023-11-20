0d34899c68b138f33f990e6fd7b268fb33f5e7f4
bakterimu <>
2023-11-20 19:15:59 +0700

    add user repository

---
+++ src/main/java/com/app/smartdrive/api/repositories/users/UserRepo.java
@@ -0,0 +1,13 @@
+package com.app.smartdrive.api.repositories.users;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.app.smartdrive.api.entities.users.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long>{
+  
+}
