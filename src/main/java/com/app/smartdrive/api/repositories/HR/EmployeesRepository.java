cd30fe17628053c517dc9ab542a0169c1366589f
muhammadbara <muhammadbara271@gmail.com>
2023-11-21 13:37:31 +0700

    update service

--- src/main/java/com/app/smartdrive/api/repositories/HR/EmployeesRepository.java
+++ src/main/java/com/app/smartdrive/api/repositories/HR/EmployeesRepository.java
@@ -1,6 +1,10 @@
 package com.app.smartdrive.api.repositories.HR;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.app.smartdrive.api.entities.hr.Employees;
@@ -8,4 +12,6 @@ import com.app.smartdrive.api.entities.hr.Employees;
 @Repository
 public interface EmployeesRepository  extends JpaRepository<Employees, Long>{
     
+    @Query("SELECT e FROM Employees e WHERE e.empName = :employeeName")
+    List<Employees> findAllByEmployeeName(@Param("employeeName") String employeeName);
 }
