7b4e8d102ce355fc88ba09e956e4e027ea8756f0
bakterimu <>
2023-12-02 20:37:31 +0700

    add userphone service with dto

--- src/main/java/com/app/smartdrive/api/services/users/UserPhoneService.java
+++ src/main/java/com/app/smartdrive/api/services/users/UserPhoneService.java
@@ -1,10 +1,16 @@
 package com.app.smartdrive.api.services.users;
 
+import java.util.List;
 import com.app.smartdrive.api.dto.user.response.UserPhoneDto;
+import com.app.smartdrive.api.entities.users.User;
 import com.app.smartdrive.api.entities.users.UserPhone;
 
 public interface UserPhoneService {
   UserPhone updateUserPhone(Long id, String phoneNumber, UserPhoneDto userPost);
+
   UserPhone addUserPhone(Long id, UserPhoneDto userPost);
+
+  List<UserPhone> createUserPhone(User user, List<UserPhoneDto> userPost);
+
   void deleteUserPhone(Long id, String number);
 }
