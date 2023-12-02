891df8fa792bc431062d41eb4ffb0e300bff9d74
Ichwan <ichwansholihin03@gmail.com>
2023-12-02 19:59:50 +0700

    breaking change: controller and dto service orders

--- src/main/java/com/app/smartdrive/api/controllers/service_order/servorder/ServOrderTaskController.java
+++ src/main/java/com/app/smartdrive/api/controllers/service_order/servorder/ServOrderTaskController.java
@@ -1,21 +1,17 @@
 package com.app.smartdrive.api.controllers.service_order.servorder;
 
+import com.app.smartdrive.api.Exceptions.UserNotFoundException;
 import com.app.smartdrive.api.dto.service_order.response.SoTasksDto;
-import com.app.smartdrive.api.dto.service_order.response.SoWorkorderDto;
 import com.app.smartdrive.api.entities.service_order.ServiceOrderTasks;
-import com.app.smartdrive.api.entities.service_order.enumerated.EnumModuleServiceOrders;
 import com.app.smartdrive.api.services.service_order.servorder.ServOrderTaskService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.stream.Stream;
+import java.util.List;
 
 @RestController
 @RequestMapping("/seot")
@@ -23,29 +19,28 @@ import java.util.stream.Stream;
 @Slf4j
 public class ServOrderTaskController {
 
-    private ServOrderTaskService servOrderTaskService;
+    private final ServOrderTaskService servOrderTaskService;
 
-    @Transactional
-    @GetMapping("/servordertask")
-    public ResponseEntity<?> getServiceOrderTasksById(@RequestParam("seotid") Long seotId){
-        ServiceOrderTasks serviceOrderTasks = servOrderTaskService.findSeotById(seotId);
-
-        Stream<SoWorkorderDto> sowoStream = serviceOrderTasks.getServiceOrderWorkorders()
-                .stream()
-                .map(serviceOrderWorkorder -> SoWorkorderDto.builder()
-                        .sowoName(serviceOrderWorkorder.getSowoName())
-                        //.sowoStatus(serviceOrderWorkorder.getSowoStatus())
-                        .sowoSeotId(seotId).build());
-
-        SoTasksDto soTasksDto = SoTasksDto.builder()
-                .seotId(serviceOrderTasks.getSeotId())
-                .seotName(serviceOrderTasks.getSeotName())
-                .seotStatus(EnumModuleServiceOrders.SeotStatus.valueOf(serviceOrderTasks.getSeotStatus()))
-                .seotStartDate(serviceOrderTasks.getSeotStartDate())
-                .seotEndDate(serviceOrderTasks.getSeotEndDate())
-                .serviceOrderWorkorders(sowoStream).build();
-
+    @GetMapping()
+    public ResponseEntity<?> getServiceOrderTasksById(@RequestParam("seroId") String seroId){
+        List<ServiceOrderTasks> seotBySeroId = servOrderTaskService.findSeotBySeroId(seroId);
         log.info("ServiceOrdersController::getServiceOrderTasksById successfully viewed");
+        return new ResponseEntity<>(seotBySeroId, HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> updateSeotStatus(@Valid @RequestBody SoTasksDto soTasksDto){
+        int updated = servOrderTaskService.updateTasksStatus(soTasksDto.getSeotStatus(), soTasksDto.getSeotId());
+
+        if (updated == 0){
+            throw new UserNotFoundException("ID Not Found");
+        }
+
+        soTasksDto = SoTasksDto.builder()
+                .seotName(soTasksDto.getSeotName())
+                .seotStatus(soTasksDto.getSeotStatus())
+                .seotId(soTasksDto.getSeotId()).build();
+
         return new ResponseEntity<>(soTasksDto, HttpStatus.OK);
     }
 }
