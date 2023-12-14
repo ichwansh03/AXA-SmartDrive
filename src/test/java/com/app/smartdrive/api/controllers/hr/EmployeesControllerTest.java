dd27762d45110db7738051ef20241cbc3c8d72ff
muhammadbara <muhammadbara271@gmail.com>
2023-12-14 14:21:26 +0700

    unit test HR

---
+++ src/test/java/com/app/smartdrive/api/controllers/hr/EmployeesControllerTest.java
@@ -0,0 +1,19 @@
+package com.app.smartdrive.api.controllers.hr;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeesControllerTest {
+    
+    @Autowired
+    private MockMvc mockMvc;
+
+
+}
