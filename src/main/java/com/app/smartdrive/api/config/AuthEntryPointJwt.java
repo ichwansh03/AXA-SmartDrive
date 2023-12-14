5b3b4146d895ca700372ddc09847663e6078834d
bakterimu <>
2023-12-14 11:50:05 +0700

    delete unused import

--- src/main/java/com/app/smartdrive/api/config/AuthEntryPointJwt.java
+++ src/main/java/com/app/smartdrive/api/config/AuthEntryPointJwt.java
@@ -5,15 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
