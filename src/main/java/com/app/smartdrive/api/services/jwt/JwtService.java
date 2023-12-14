4b7f8b3697734ae0a5a5dca11bb60d9063f11818
bakterimu <>
2023-12-14 11:49:35 +0700

    fix refresh token and sign out

--- src/main/java/com/app/smartdrive/api/services/jwt/JwtService.java
+++ src/main/java/com/app/smartdrive/api/services/jwt/JwtService.java
@@ -1,20 +1,29 @@
 package com.app.smartdrive.api.services.jwt;
 
+import com.app.smartdrive.api.entities.users.EnumUsers;
 import com.app.smartdrive.api.entities.users.User;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 public interface JwtService {
   String extractUserName(String token);
   String generateToken(UserDetails userDetails);
   boolean isTokenValid(String token, UserDetails userDetails);
 
-  String getJwtFromCookies(HttpServletRequest request);
+  boolean isTokenExpired(String token);
+
+  String getJwtFromCookies(HttpServletRequest request, String cookieName);
 
   ResponseCookie generateJwtCookie(User userPrincipal);
 
-  ResponseCookie getCleanJwtCookie();
+  ResponseCookie getCleanJwtCookie(String cookieName, String path);
+
+  ResponseCookie generateCookie(String name, String value, String path);
+
+  boolean validateJwtToken(String authToken);
 
   ResponseCookie generateRefreshJwtCookie(String refreshToken);
 }
--- src/main/java/com/app/smartdrive/api/services/jwt/implementation/JwtServiceImpl.java
+++ src/main/java/com/app/smartdrive/api/services/jwt/implementation/JwtServiceImpl.java
@@ -2,13 +2,13 @@ package com.app.smartdrive.api.services.jwt.implementation;
 
 import com.app.smartdrive.api.entities.users.User;
 import com.app.smartdrive.api.services.jwt.JwtService;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -23,14 +23,19 @@ import java.util.function.Function;
 
 @Service
 public class JwtServiceImpl implements JwtService {
+  private static final Logger logger = LoggerFactory.getLogger(JwtServiceImpl.class);
+
   @Value("${jwt.token.signing-key}")
   private String jwtSigningKey;
-  @Value("${jwt.cookie.name}")
-  private String jwtCookie;
   @Value("${jwt.refreshExpired}")
-  private String jwtRefreshCookie;
+  private String jwtRefreshExpired;
   @Value("${jwt.expired}")
   private Long jwtExpirationMs;
+  @Value("${jwt.cookie.name}")
+  private String jwtCookie;
+
+  @Value("${jwt.refresh.cookie}")
+  private String jwtRefreshCookie;
 
   @Override
   public String extractUserName(String token) {
@@ -45,7 +50,7 @@ public class JwtServiceImpl implements JwtService {
   @Override
   public boolean isTokenValid(String token, UserDetails userDetails) {
     final String userName = extractUserName(token);
-    return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    return (userName.equals(userDetails.getUsername()));
   }
 
   private <T> T extractClaim(String token,Function<Claims, T> claimsResolvers){
@@ -54,13 +59,15 @@ public class JwtServiceImpl implements JwtService {
   }
 
   private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails){
-    return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
+    return Jwts.builder().setClaims(extraClaims).claim("role", userDetails.getAuthorities())
+            .setSubject(userDetails.getUsername())
             .setIssuedAt(new Date(System.currentTimeMillis()))
             .setExpiration(new Date(System.currentTimeMillis()+jwtExpirationMs))
             .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
   }
 
-  private boolean isTokenExpired(String token){
+  @Override
+  public boolean isTokenExpired(String token){
     return extractExpiration(token).before(new Date());
   }
 
@@ -79,8 +86,8 @@ public class JwtServiceImpl implements JwtService {
   }
 
   @Override
-  public String getJwtFromCookies(HttpServletRequest request){
-    Cookie cookie = WebUtils.getCookie(request, jwtCookie);
+  public String getJwtFromCookies(HttpServletRequest request, String cookieName){
+    Cookie cookie = WebUtils.getCookie(request, cookieName);
     if (cookie != null) {
       return cookie.getValue();
     } else {
@@ -96,19 +103,39 @@ public class JwtServiceImpl implements JwtService {
   }
 
   @Override
-  public ResponseCookie getCleanJwtCookie(){
-    ResponseCookie cookie = ResponseCookie.from(jwtCookie, null).path("/api").build();
+  public ResponseCookie getCleanJwtCookie(String cookieName, String path){
+    ResponseCookie cookie = ResponseCookie.from(cookieName, null).path(path).httpOnly(true)
+            .maxAge(0)
+            .build();
     return cookie;
   }
 
+  @Override
   public ResponseCookie generateCookie(String name, String value, String path){
     ResponseCookie cookie = ResponseCookie.from(name, value).path(path).maxAge(24*60*60).httpOnly(true).build();
     return cookie;
   }
 
+  @Override
+  public boolean validateJwtToken(String authToken) {
+    try {
+      Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parse(authToken);
+      return true;
+    } catch (MalformedJwtException e) {
+      logger.error("Invalid JWT token: {}", e.getMessage());
+    } catch (UnsupportedJwtException e) {
+      logger.error("JWT token is unsupported: {}", e.getMessage());
+    } catch (IllegalArgumentException e) {
+      logger.error("JWT claims string is empty: {}", e.getMessage());
+    }
+
+    return false;
+  }
+
   public ResponseCookie generateRefreshJwtCookie(String refreshToken){
     return generateCookie(jwtRefreshCookie, refreshToken, "/api/auth/refreshtoken");
   }
+
 
 //  public ResponseCookie generateCookie(String name, String value)
 //  private String generateTokenFromUsername(String username) {
