a9a150a1fa989e1cbd684a3bd872b4eef137348b
bakterimu <>
2023-12-15 13:22:28 +0700

    otw testing

--- src/main/java/com/app/smartdrive/api/dto/user/request/ProfileRequestDto.java
+++ src/main/java/com/app/smartdrive/api/dto/user/request/ProfileRequestDto.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.UniqueElements;
@@ -13,6 +14,7 @@ import org.hibernate.validator.constraints.UniqueElements;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ProfileRequestDto {
   private String userName;
   private String userPassword;
---
+++ src/test/java/com/app/smartdrive/api/controllers/users/UserControllerTest.java
@@ -0,0 +1,121 @@
+package com.app.smartdrive.api.controllers.users;
+
+import com.app.smartdrive.api.dto.user.request.CreateUserDto;
+import com.app.smartdrive.api.dto.user.request.ProfileRequestDto;
+import com.app.smartdrive.api.dto.user.response.UserDto;
+import com.app.smartdrive.api.entities.users.*;
+import com.app.smartdrive.api.mapper.TransactionMapper;
+import com.app.smartdrive.api.services.service_order.claims.ClaimAssetService;
+import com.app.smartdrive.api.services.users.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.access.SecurityConfig;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.File;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@Slf4j
+@WebMvcTest(UserController.class)
+@ImportAutoConfiguration(classes = {SecurityConfig.class})
+class UserControllerTest {
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @MockBean
+  private UserService userService;
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  TransactionMapper transactionMapper;
+
+  List<UserRoles> addRole(EnumUsers.RoleName roleName, User user) {
+    UserRolesId userRolesId = new UserRolesId(user.getUserEntityId(), roleName);
+
+    Roles roles = new Roles();
+    roles.setRoleDescription("customer");
+    roles.setRoleName(roleName);
+
+    UserRoles userRoles = new UserRoles();
+    userRoles.setRoles(roles);
+    userRoles.setUserRolesId(userRolesId);
+    userRoles.setUsroStatus("ACTIVE");
+    userRoles.setUsroModifiedDate(LocalDateTime.now());
+
+    List<UserRoles> listRole = new ArrayList<>();
+    listRole.add(userRoles);
+    user.setUserRoles(listRole);
+    return listRole;
+  }
+
+  User createUser() throws Exception{
+    CreateUserDto createUserDto = objectMapper.readValue(
+            new File("src/main/resources/request/CreateUserDto.json"),
+            CreateUserDto.class);
+    User user = new User();
+    user.setUserEntityId(1L);
+    user.setUserPhoto("test.png");
+    TransactionMapper.mapDtoToEntity(createUserDto.getProfile(), user);
+    TransactionMapper.mapDtoToEntity(createUserDto, user);
+
+    addRole(EnumUsers.RoleName.CU, user);
+
+    return user;
+  }
+
+  @Test
+  @WithMockUser(authorities = {"Admin"})
+  void shouldReturnUser() throws Exception{
+    Long id = 1L;
+    User user = createUser();
+    UserDto userDto = TransactionMapper.mapEntityToDto(user, UserDto.class);
+    when(userService.getById(1L)).thenReturn(user);
+    mockMvc.perform(get("/user/{id}", id))
+            .andExpect(jsonPath("$.userEntityId").value(id))
+            .andExpect(jsonPath("$.userPhoto").value(userDto.getUserPhoto()))
+            .andExpect(jsonPath("$.userFullName").value(userDto.getUserFullName()))
+            .andExpect(jsonPath("$.userEmail").value(userDto.getUserEmail()))
+            .andExpect(jsonPath("$.userAddress").value(userDto.getUserAddress()))
+            .andExpect(jsonPath("$.userPhone").value(userDto.getUserPhone()))
+            .andExpect(jsonPath("$.userAccounts").value(userDto.getUserPhone()))
+            .andExpect(jsonPath("$.userRoles").value(userDto.getUserRoles()))
+            .andDo(print());
+
+  }
+
+
+  void testCreateUser() throws Exception {
+    CreateUserDto createUserDto = objectMapper.readValue(
+            new File("src/main/resources/request/CreateUserDto.json"),
+            CreateUserDto.class);
+    User user = new User();
+    user.setUserEntityId(1L);
+    user.setUserPhoto("test.png");
+    TransactionMapper.mapDtoToEntity(createUserDto.getProfile(), user);
+    TransactionMapper.mapDtoToEntity(createUserDto, user);
+//    user.setUserAddress(userAddress);
+//    TransactionMapper.mapDtoToEntity(createUserDto.getUserAccounts(), user.getUserAccounts());
+//    TransactionMapper.mapDtoToEntity(createUserDto.getUserPhone(), user.getUserPhone());
+
+    addRole(EnumUsers.RoleName.CU, user);
+  }
+}
