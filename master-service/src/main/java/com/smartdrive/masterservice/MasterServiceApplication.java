bc56d204ef0bb587cab35363bcd07a63bff38657
Tengku1 <anountengku@gmail.com>
2023-12-15 14:43:37 +0700

    Set Kafka Producer at Master-Service

--- master-service/src/main/java/com/smartdrive/masterservice/MasterServiceApplication.java
+++ master-service/src/main/java/com/smartdrive/masterservice/MasterServiceApplication.java
@@ -2,10 +2,8 @@ package com.smartdrive.masterservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class MasterServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MasterServiceApplication.class, args);
