ea54b037726a5562f026b96610d09bebf92c014c
MichaelGerrardStanleyH <hutagalung.michael23@gmail.com>
2023-12-16 14:29:49 +0700

    create testing for all create customer request

--- src/test/java/com/app/smartdrive/api/controllers/customerService/customer/CustomerServiceControllerTest.java
+++ src/test/java/com/app/smartdrive/api/controllers/customerService/customer/CustomerServiceControllerTest.java
@@ -1,7 +1,11 @@
 package com.app.smartdrive.api.controllers.customerService.customer;
 
 import com.app.smartdrive.api.Exceptions.EntityNotFoundException;
+import com.app.smartdrive.api.dto.customer.request.CiasDTO;
+import com.app.smartdrive.api.dto.customer.request.CreateCustomerRequestByAgenDTO;
+import com.app.smartdrive.api.dto.customer.request.CustomerRequestDTO;
 import com.app.smartdrive.api.dto.customer.response.CustomerResponseDTO;
+import com.app.smartdrive.api.dto.user.request.CreateUserDto;
 import com.app.smartdrive.api.entities.customer.CustomerRequest;
 import com.app.smartdrive.api.entities.hr.EmployeeAreaWorkgroup;
 import com.app.smartdrive.api.entities.master.AreaWorkGroup;
@@ -15,6 +19,7 @@ import com.app.smartdrive.api.services.master.CityService;
 import com.app.smartdrive.api.services.master.IntyService;
 import com.app.smartdrive.api.services.users.BusinessEntityService;
 import com.app.smartdrive.api.services.users.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -25,9 +30,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.*;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -246,5 +259,215 @@ public class CustomerServiceControllerTest {
         ).andDo(print());
     }
 
+    @Test
+    void create_willSuccess() throws Exception {
+        MockMultipartFile file =
+                new MockMultipartFile(
+                        "file",
+                        "contract.pdf",
+                        MediaType.APPLICATION_PDF_VALUE,
+                        "<<pdf data>>".getBytes(StandardCharsets.UTF_8));
 
+        MultipartFile[] multipartFiles = {file};
+
+
+
+        CustomerRequestDTO customerRequestDTO = CustomerRequestDTO.builder()
+                .employeeId(1L)
+                .agenId(1L)
+                .customerId(2L)
+                .build();
+
+        CustomerResponseDTO customerResponseDTO = CustomerResponseDTO.builder()
+                .creqEntityId(customerRequestDTO.getCustomerId())
+                .build();
+
+        when(this.customerRequestService.create(customerRequestDTO, multipartFiles))
+                .thenReturn(customerResponseDTO);
+
+        mockMvc.perform(multipart("/customer/service/request")
+                .file(file)
+                .param("client", this.objectMapper.writeValueAsString(customerRequestDTO))
+        ).andExpect(status().isCreated()
+        ).andDo(result -> {
+            CustomerResponseDTO response = this.objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+            });
+
+            assertNotNull(response);
+            assertEquals(customerResponseDTO, response);
+        });
+    }
+
+    @Test
+    void create_willFailed() throws Exception {
+        MockMultipartFile file =
+                new MockMultipartFile(
+                        "file",
+                        "contract.pdf",
+                        MediaType.APPLICATION_PDF_VALUE,
+                        "<<pdf data>>".getBytes(StandardCharsets.UTF_8));
+
+        MultipartFile[] multipartFiles = {file};
+
+
+
+        CustomerRequestDTO customerRequestDTO = CustomerRequestDTO.builder()
+                .employeeId(1L)
+                .agenId(1L)
+                .customerId(2L)
+                .build();
+
+        CustomerResponseDTO customerResponseDTO = CustomerResponseDTO.builder()
+                .creqEntityId(customerRequestDTO.getCustomerId())
+                .build();
+
+        when(this.customerRequestService.create(customerRequestDTO, multipartFiles))
+                .thenThrow(new EntityNotFoundException("User with id " + customerRequestDTO.getCustomerId() + " is not found"));
+
+        mockMvc.perform(multipart("/customer/service/request")
+                .file(file)
+                .param("client", this.objectMapper.writeValueAsString(customerRequestDTO))
+        ).andExpect(status().isNotFound()
+        ).andExpect(result -> assertTrue(result.getResolvedException() instanceof EntityNotFoundException)
+        ).andExpect(result -> assertEquals("User with id " + customerRequestDTO.getCustomerId() + " is not found", result.getResolvedException().getMessage())
+        ).andDo(print());
+    }
+
+    @Test
+    void createByAgen_willSuccess() throws Exception {
+        MockMultipartFile file =
+                new MockMultipartFile(
+                        "file",
+                        "contract.pdf",
+                        MediaType.APPLICATION_PDF_VALUE,
+                        "<<pdf data>>".getBytes(StandardCharsets.UTF_8));
+
+        MultipartFile[] multipartFiles = {file};
+
+
+
+        CreateCustomerRequestByAgenDTO createCustomerRequestByAgenDTO = CreateCustomerRequestByAgenDTO.builder()
+                .employeeId(1L)
+                .agenId(1L)
+                .accessGrantUser(true)
+                .build();
+
+        CustomerResponseDTO customerResponseDTO = CustomerResponseDTO.builder()
+                .creqEntityId(1L)
+                .build();
+
+
+
+
+        when(this.customerRequestService.createByAgen(createCustomerRequestByAgenDTO, multipartFiles))
+                .thenReturn(customerResponseDTO);
+
+        mockMvc.perform(multipart("/customer/service/request/agen")
+                .file(file)
+                .param("client", this.objectMapper.writeValueAsString(createCustomerRequestByAgenDTO))
+        ).andExpect(status().isCreated()
+        ).andDo(result -> {
+            CustomerResponseDTO response = this.objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+            });
+
+            assertNotNull(response);
+            assertEquals(customerResponseDTO, response);
+        });
+    }
+
+    @Test
+    void createByAgen_willFailed() throws Exception {
+        MockMultipartFile file =
+                new MockMultipartFile(
+                        "file",
+                        "contract.pdf",
+                        MediaType.APPLICATION_PDF_VALUE,
+                        "<<pdf data>>".getBytes(StandardCharsets.UTF_8));
+
+        MultipartFile[] multipartFiles = {file};
+
+
+        CiasDTO ciasDTO = CiasDTO.builder()
+                .ciasPoliceNumber("B 1234 CBD")
+                .build();
+
+        CreateCustomerRequestByAgenDTO createCustomerRequestByAgenDTO = CreateCustomerRequestByAgenDTO.builder()
+                .employeeId(1L)
+                .agenId(1L)
+                .accessGrantUser(true)
+                .ciasDTO(ciasDTO)
+                .build();
+
+        CustomerResponseDTO customerResponseDTO = CustomerResponseDTO.builder()
+                .creqEntityId(1L)
+                .build();
+
+
+
+
+        when(this.customerRequestService.createByAgen(createCustomerRequestByAgenDTO, multipartFiles))
+                .thenThrow(new Exception("CustomerRequest with police number " + createCustomerRequestByAgenDTO.getCiasDTO().getCiasPoliceNumber() + " is already exist"));
+
+        mockMvc.perform(multipart("/customer/service/request/agen")
+                .file(file)
+                .param("client", this.objectMapper.writeValueAsString(createCustomerRequestByAgenDTO))
+        ).andExpect(status().isInternalServerError()
+        ).andExpect(result -> assertTrue(result.getResolvedException() instanceof Exception)
+        ).andExpect(result -> assertEquals(
+                "CustomerRequest with police number " + createCustomerRequestByAgenDTO.getCiasDTO().getCiasPoliceNumber() + " is already exist",
+                result.getResolvedException().getMessage())
+        ).andDo(print());
+    }
+
+
+
+    @Test
+    void update_willSuccess() throws Exception {
+        MockMultipartFile file =
+                new MockMultipartFile(
+                        "file",
+                        "contract.pdf",
+                        MediaType.APPLICATION_PDF_VALUE,
+                        "<<pdf data>>".getBytes(StandardCharsets.UTF_8));
+
+        MultipartFile[] multipartFiles = {file};
+
+
+
+        CustomerRequestDTO customerRequestDTO = CustomerRequestDTO.builder()
+                .employeeId(1L)
+                .agenId(1L)
+                .customerId(2L)
+                .build();
+
+        CustomerResponseDTO customerResponseDTO = CustomerResponseDTO.builder()
+                .creqEntityId(customerRequestDTO.getCustomerId())
+                .build();
+
+        // custom http method for multipart
+        MockMultipartHttpServletRequestBuilder builder =
+                MockMvcRequestBuilders.multipart("/customer/service/request");
+        builder.with(new RequestPostProcessor() {
+            @Override
+            public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                request.setMethod("PUT");
+                return request;
+            }
+        });
+
+        when(this.customerRequestService.create(customerRequestDTO, multipartFiles))
+                .thenReturn(customerResponseDTO);
+
+        mockMvc.perform(multipart("/customer/service/request")
+                .file(file)
+                .param("client", this.objectMapper.writeValueAsString(customerRequestDTO))
+        ).andExpect(status().isCreated()
+        ).andDo(result -> {
+            CustomerResponseDTO response = this.objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+            });
+
+            assertNotNull(response);
+            assertEquals(customerResponseDTO, response);
+        });
+    }
 }
