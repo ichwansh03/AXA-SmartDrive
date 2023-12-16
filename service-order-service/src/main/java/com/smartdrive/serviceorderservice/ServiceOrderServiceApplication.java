394655830fbf5e633a0e55aab0b4a49438c7293f
Tengku1 <anountengku@gmail.com>
2023-12-16 21:58:16 +0700

    Add Service Order Microservice

---
+++ service-order-service/src/main/java/com/smartdrive/serviceorderservice/ServiceOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smartdrive.serviceorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceOrderServiceApplication.class, args);
+	}
+
+}
