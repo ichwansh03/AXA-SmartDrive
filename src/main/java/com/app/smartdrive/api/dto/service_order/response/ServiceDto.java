369a21238314fdc7c986e0e6638f20a6210ccfad
Ichwan <ichwansholihin03@gmail.com>
2024-01-02 11:34:41 +0700

    breaking change: extend class to get service ID

--- src/main/java/com/app/smartdrive/api/dto/service_order/response/ServiceDto.java
+++ src/main/java/com/app/smartdrive/api/dto/service_order/response/ServiceDto.java
@@ -1,20 +1,12 @@
 package com.app.smartdrive.api.dto.service_order.response;
 
-import com.app.smartdrive.api.entities.customer.EnumCustomer;
-import com.app.smartdrive.api.entities.service_order.enumerated.EnumModuleServiceOrders;
-import lombok.Data;
+import com.app.smartdrive.api.dto.service_order.request.ServiceReqDto;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.time.LocalDateTime;
-
-@Data
-public class ServiceDto {
+@Getter
+@Setter
+public class ServiceDto extends ServiceReqDto {
 
     private Long servId;
-    private EnumCustomer.CreqType servType;
-    private String servInsuranceNo;
-    private String servVehicleNumber;
-    private LocalDateTime servCreatedOn;
-    private LocalDateTime servStartDate;
-    private LocalDateTime servEndDate;
-    private EnumModuleServiceOrders.ServStatus servStatus;
 }
