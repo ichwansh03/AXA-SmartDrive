18f0be26a54e4dc4bd7c5a41f7ed37eccd32230f
Ichwan <ichwansholihin03@gmail.com>
2024-01-05 09:12:29 +0700

    breaking change: remote multiple delete mapping

--- src/main/java/com/app/smartdrive/api/controllers/master/CateController.java
+++ src/main/java/com/app/smartdrive/api/controllers/master/CateController.java
@@ -46,29 +46,4 @@ public class CateController implements MasterController<CateReq, Long> {
         return new ResponseEntity<>(cateServiceImpl.save(request), HttpStatus.CREATED);
     }
 
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteData(@PathVariable Long id) {
-        cateServiceImpl.deleteById(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteData(@PathVariable Long id){
-        cateServiceImpl.deleteById(id);
-        return ResponseEntity.ok("deleted");
-    }
-
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteData(@PathVariable Long id) {
-        cateServiceImpl.deleteById(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteData(@PathVariable Long id){
-        cateServiceImpl.deleteById(id);
-        return ResponseEntity.ok("deleted");
-    }
 }
