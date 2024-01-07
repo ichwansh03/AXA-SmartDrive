d974578a55a2a81a6a1280183d947343ff3157ca
Ichwan <ichwansholihin03@gmail.com>
2024-01-07 18:40:53 +0700

    breaking change/fix: delete unused repository method

--- src/main/java/com/app/smartdrive/api/repositories/service_orders/SecrRepository.java
+++ src/main/java/com/app/smartdrive/api/repositories/service_orders/SecrRepository.java
@@ -1,25 +1,13 @@
 package com.app.smartdrive.api.repositories.service_orders;
 
-import com.app.smartdrive.api.entities.payment.PaymentTransactions;
 import com.app.smartdrive.api.entities.service_order.ServicePremiCredit;
 import com.app.smartdrive.api.entities.service_order.ServicePremiCreditId;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Repository
 public interface SecrRepository extends JpaRepository<ServicePremiCredit, ServicePremiCreditId> {
-
-    List<ServicePremiCredit> findByServices_ServId(Long servId);
 
     ServicePremiCredit findBySecrDuedateBetween(LocalDateTime startDate, LocalDateTime endDate);
 
