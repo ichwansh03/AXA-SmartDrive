954454e21c04b86fb555859cea3d472aea1bc702
Ichwan <ichwansholihin03@gmail.com>
2024-01-07 18:45:11 +0700

    breaking change: disable pre authorize

--- src/main/java/com/app/smartdrive/api/controllers/service_order/servorder/ServOrderTaskController.java
+++ src/main/java/com/app/smartdrive/api/controllers/service_order/servorder/ServOrderTaskController.java
@@ -2,6 +2,7 @@ package com.app.smartdrive.api.controllers.service_order.servorder;
 
 import com.app.smartdrive.api.dto.service_order.request.SeotPartnerDto;
 import com.app.smartdrive.api.dto.service_order.response.SoTasksDto;
+import com.app.smartdrive.api.services.service_order.servorder.ServOrderTaskService;
 import com.app.smartdrive.api.services.service_order.servorder.ServiceTasksFactory;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -11,19 +12,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequestMapping("/service/task")
 @RequiredArgsConstructor
 @Slf4j
+@CrossOrigin
 public class ServOrderTaskController {
 
-    private final ServiceTasksFactory serviceTasksFactory;
+    private final ServOrderTaskService servOrderTaskService;
+    private final ServiceTasksFactory serviceTasksTransaction;
 
-    @PutMapping("/update/{seotId}")
-    @PreAuthorize("hasAuthority('Employee') || hasAuthority('Admin')")
+    @GetMapping("{seotId}")
+    public ResponseEntity<?> getTaskById(@PathVariable("seotId") Long seotId) {
+        SoTasksDto taskById = servOrderTaskService.findTaskById(seotId);
+        return new ResponseEntity<>(taskById, HttpStatus.OK);
+    }
+
+    @PutMapping("/{seotId}")
+    //@PreAuthorize("hasAuthority('Employee') || hasAuthority('Admin')")
     public ResponseEntity<?> updateSeotStatus(@Valid @RequestBody SoTasksDto soTasksDto, @PathVariable("seotId") Long seotId) {
-        int updated = serviceTasksFactory.updateTasksStatus(soTasksDto.getSeotStatus(), seotId);
+        int updated = serviceTasksTransaction.updateTasksStatus(soTasksDto.getSeotStatus(), seotId);
 
         return new ResponseEntity<>(updated, HttpStatus.OK);
     }
@@ -32,9 +40,9 @@ public class ServOrderTaskController {
     @PreAuthorize("hasAuthority('Employee') || hasAuthority('Admin')")
     public ResponseEntity<?> updateSeotStatus(@Valid @RequestBody SeotPartnerDto soTasksDto, @PathVariable("seotId") Long seotId) {
 
-        serviceTasksFactory.updateSeotPartner(soTasksDto, seotId);
+        serviceTasksTransaction.updateSeotPartner(soTasksDto, seotId);
 
-        serviceTasksFactory.updateTasksStatus(soTasksDto.getSeotStatus(), seotId);
+        serviceTasksTransaction.updateTasksStatus(soTasksDto.getSeotStatus(), seotId);
 
         return new ResponseEntity<>(soTasksDto, HttpStatus.OK);
     }
