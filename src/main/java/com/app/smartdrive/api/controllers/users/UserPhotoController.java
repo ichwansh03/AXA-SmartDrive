618514b8c8c48f7a8ab8f4592f4a1e26c3669557
bakterimu <>
2024-01-08 07:52:49 +0700

    fix user-photos to show in front end

--- src/main/java/com/app/smartdrive/api/controllers/users/UserPhotoController.java
+++ src/main/java/com/app/smartdrive/api/controllers/users/UserPhotoController.java
@@ -1,33 +1,68 @@
 package com.app.smartdrive.api.controllers.users;
 
 import com.app.smartdrive.api.dto.auth.response.MessageResponse;
+import com.app.smartdrive.api.entities.users.User;
+import com.app.smartdrive.api.services.users.FileStorageService;
+import com.app.smartdrive.api.services.users.UserPhotoService;
+import com.app.smartdrive.api.services.users.UserService;
+import com.app.smartdrive.api.services.users.implementation.PhotoUserStorageServiceImpl;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import com.app.smartdrive.api.dto.user.response.UserPhoneDto;
-import com.app.smartdrive.api.entities.users.UserPhone;
-import com.app.smartdrive.api.services.users.UserPhotoService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import lombok.RequiredArgsConstructor;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @PreAuthorize("isAuthenticated() && (hasAuthority('Admin') || principal.user.getUserEntityId() == #id)")
 @RequiredArgsConstructor
+@CrossOrigin(allowCredentials = "true",allowedHeaders = {"Authorization", "Content-Type", "Origin"}, origins = "http://localhost:3006", maxAge = 2592000)
 @RequestMapping("/user/{id}/photo")
 public class UserPhotoController {
   
   private final UserPhotoService userPhotoService;
+  private final UserService userService;
+  private final Path pathPhoto = Paths.get("C:\\Izhar\\AXA-SmartDrive\\assets\\UserPhotos").toAbsolutePath().normalize();
+  private final FileStorageService photoUserStorageServiceImpl = new PhotoUserStorageServiceImpl(pathPhoto);
 
   @PostMapping
   public ResponseEntity<?> savePhotoUser(@RequestParam MultipartFile photo, @PathVariable("id") Long id) throws Exception{
-    userPhotoService.savePhoto(photo, id);
-    return ResponseEntity.status(HttpStatus.CREATED).body(new MessageResponse("Success save photo user"));
+    String photoName = userPhotoService.savePhoto(photo, id);
+    return ResponseEntity.status(HttpStatus.CREATED).body(new MessageResponse(photoName));
+  }
+
+  @GetMapping
+  @ResponseBody
+  public ResponseEntity<?> getImageDynamicType(@PathVariable("id") Long id, HttpServletRequest request) {
+    User user = userService.getById(id);
+    String photoName = user.getUserPhoto();
+    Resource resource = photoUserStorageServiceImpl.loadFileAsResource(photoName);
+    String contentType = null;
+
+    try {
+      contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    if (contentType == null) {
+      contentType = "application/octet-stream";
+    }
+
+    return ResponseEntity.ok()
+            .contentType(MediaType.parseMediaType(contentType))
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+            .body(resource);
   }
 }
